7347fbdd9f8fff421a3a826d65d2bee1d8b3c43d
Ricsy088 <80894618+Ricsy088@users.noreply.github.com>
2021-11-17 10:52:35 +0100

    Add files via upload

---
+++ vissza.py
@@ -0,0 +1,20 @@
+list = ["Karcsi", "Iren", "Zoli","Gaspar", "Győző"]
+nev = input ('Adj egy mevet: ')
+if nev in list:
+    print ('Benne van')
+else:
+    print ('Nincs benne')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
